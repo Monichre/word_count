967712913d787c140153ca8b6424be7bd3bc97a8
Liam Ellis <liamhellis@gmail.com>
2016-07-01 10:04:56 -0700

    second test passing, not sure how many more tests are really neccessary

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -6,6 +6,6 @@ describe('String#word_count') do
     expect(('the arsonist had oddly shaped feet').word_count('the')).to(eq(1))
   end
   it('returns the number the number of times a word appears in a given string') do
-    expect(('the arsonist had oddly shaped feet. Thus, the shoes he wore were the oddest Id ever seen).word_count('the')).to(eq(3))
+    expect(('the arsonist had oddly shaped feet. Thus, the shoes he wore were the oddest Id ever seen').word_count('the')).to(eq(3))
   end
 end
