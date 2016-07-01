58c7b2127ce74b5cfe7d06c2e9c0775521adbdda
Liam Ellis <liamhellis@gmail.com>
2016-07-01 10:31:41 -0700

    full integration specs in place, views folder up

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -6,7 +6,8 @@ set(:show_exceptions, false)
 descrbe('the index path', {:type => :feature}) do
   it('processes the user entry and returns the number of times a given word appears in a sentence') do
     visit('/')
-    fill_in('user_entry', :with => 'The arsonist had oddly shaped feet')
+    fill_in('user_input_string', :with => 'The arsonist had oddly shaped feet')
+    fill_in('user_input_word', :with => 'The')
     click_button('send')
     expect(page).to have_content('1')
   end
