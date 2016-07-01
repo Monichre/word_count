21dad6bfb9d02d13565911385480714f6bd3b3e2
Liam Ellis <liamhellis@gmail.com>
2016-07-01 11:00:36 -0700

    integration test passing, all tests passing

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require('sinatra')
 require('sinatra/reloader')
+require('./lib/word_count')
+also_reload('lib/**/*.rb')
 
 get('/')do
   erb(:index)
+end
+
+get('/word_count') do
+  @user_input_string = params.fetch('user_input_string')
+  @user_input_word = params.fetch('user_input_word')
+  @search_result = @user_input_string.word_count(@user_input_word)
+  erb(:result)
 end
--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -1,14 +1,14 @@
-require('capybara/rpsec')
+require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-descrbe('the index path', {:type => :feature}) do
+describe('the index path', {:type => :feature}) do
   it('processes the user entry and returns the number of times a given word appears in a sentence') do
     visit('/')
     fill_in('user_input_string', :with => 'The arsonist had oddly shaped feet')
     fill_in('user_input_word', :with => 'The')
-    click_button('send')
+    click_button('Send')
     expect(page).to have_content('1')
   end
 end
