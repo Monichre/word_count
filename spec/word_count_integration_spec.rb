951c565c7bc6a902fd4640b83e234f2482c22bf2
Liam Ellis <liamhellis@gmail.com>
2016-07-01 11:44:46 -0700

    all tests passing

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -11,4 +11,9 @@ describe('the index path', {:type => :feature}) do
     click_button('Send')
     expect(page).to have_content('1')
   end
+  it('returns to home page when link is clicked') do
+    visit('/word_count')
+    click_link('#home')
+    expect(page).to have_content('The future of word parsing is here. Enter your document, and your search term parameters')
+  end
 end
